da5dc4b6f4985765345cf22424148191ff1659f3
Alec Cox <avlecxk@gmail.com>
2020-08-30 15:36:52 -0700

    Updated for use of header.
    CMakeLists sees header.
    Test includes header.

--- test.cpp
+++ test.cpp
@@ -1,4 +1,8 @@
-#include <string>
-int main() {
-	constexpr std::string x("fffff");
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "include/argparse.hpp"
+
+TEST_CASE("AHHHHHHHHH", "[test]") {
+    REQUIRE(1 == 1);
 }
